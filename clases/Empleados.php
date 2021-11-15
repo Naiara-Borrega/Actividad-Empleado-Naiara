2cc4c51f38d7a45765dac8b8c2def3d333bbdffd
Naiara Borrega Oyaregui <nborregoya1@educacion.navarra.es>
2021-11-15 22:07:07 +0100

    Naiara: Clase Empleado modificada y pruebas de los metodos de la clase Empleado echas

--- clases/Empleados.php
+++ clases/Empleados.php
@@ -1,3 +1,41 @@
 <?php
-    
+    class Empleado{
+        private $nombre;
+        private $apellido;
+        private $numeroSeguridadSocial;
+
+        public function __construct($nombre, $apellido, $numeroSeguridadSocial){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        public function getApellido(){
+            return $this->apellido;
+        }
+
+        public function getNumeroSeguridadSocial(){
+            return $this->numeroSeguridadSocial;
+        }
+
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function setApellido($apellido){
+            $this->apellido = $apellido;
+        }
+
+        public function setNumeroSeguridadSocial($numeroSeguridadSocial){
+            $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+        }
+
+        public function mostrar(){
+            echo "Este empleado $this->nombre $this->apellido con el NSS: $this->numeroSeguridadSocial <br>"; 
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    include "vistas/vista_resultado.php"
+    include "vistas/vista_resultado.php";
+    include "clases/Empleados.php";
+
+    $empleado = new Empleado("Lola", "Etxegaray", "14-78451589-75");
+    $empleado2 = new Empleado("Maria", "Benabide", "18-56595475-56");
+
+    echo $empleado->mostrar();
+    echo $empleado2->mostrar();
 ?>
